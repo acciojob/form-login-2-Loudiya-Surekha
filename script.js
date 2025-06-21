33911608376426c33f79c2a3dc2043490e41852e
Loudiya Surekha <lsurekha2002@gmail.com>
2025-06-22 00:38:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
- document.getElementById("userForm").addEventListener("submit", function(event){
-            event.preventDefault();
-
+  const abc = (event) => {
             const firstName = document.getElementById("firstName").value;
             const lastName = document.getElementById("lastName").value;
             const phoneNumber = document.getElementById("phoneNumber").value;
             const emailId = document.getElementById("emailId").value;
 
-
-            const message=`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`;
+            const message = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`;
             alert(message);
-           })
+        };
